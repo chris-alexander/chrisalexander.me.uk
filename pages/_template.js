7e9e0c9218c21ff437b02c37b7307de7a275bc16
Chris Alexander <2815297+chris-alexander@users.noreply.github.com>
2016-10-08 19:37:07 +0100

    convert Template component

--- pages/_template.js
+++ pages/_template.js
@@ -1,38 +1,36 @@
-import React, {Component, PropTypes} from 'react';
+import React, {PropTypes} from 'react';
 import {Link} from 'react-router';
 import {Container} from 'react-responsive-grid';
 import {prefixLink} from 'gatsby-helpers';
 import {config} from 'config';
 
-class Template extends Component {
-  render() {
-    const {location, children} = this.props;
-    let header;
-    if (location.pathname === prefixLink('/')) {
-      header = (
-        <h1>
-          <Link to={prefixLink('/')}>
-            {config.blogTitle}
-          </Link>
-        </h1>
-      );
-    } else {
-      header = (
-        <h3>
-          <Link to={prefixLink('/')}>
-            {config.blogTitle}
-          </Link>
-        </h3>
-      );
-    }
-    return (
-      <Container>
-        {header}
-        {children}
-      </Container>
+const Template = props => {
+  const {location, children} = props;
+  let header;
+  if (location.pathname === prefixLink('/')) {
+    header = (
+      <h1>
+        <Link to={prefixLink('/')}>
+          {config.blogTitle}
+        </Link>
+      </h1>
+    );
+  } else {
+    header = (
+      <h3>
+        <Link to={prefixLink('/')}>
+          {config.blogTitle}
+        </Link>
+      </h3>
     );
   }
-}
+  return (
+    <Container>
+      {header}
+      {children}
+    </Container>
+  );
+};
 
 Template.propTypes = {
   children: PropTypes.any,
