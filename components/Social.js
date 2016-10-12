85ddeb66416a7ed68090c2ce2bbc1484e03141cc
Chris Alexander <2815297+chris-alexander@users.noreply.github.com>
2016-10-13 00:44:38 +0100

    tidy up

---
+++ components/Social.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {config} from 'config';
+
+const Social = () => (
+  <ul>
+    <li><a href={`//twitter.com/${config.social.twitter}`} target="_blank"></a></li>
+  </ul>
+);
+
+export default Social;
+
+// div.social
+//   ul.nav__list
+//     li.nav__item
+//       a(href="//twitter.com/#{ social.twitter }", target="_blank"): i.fa.fa-twitter
+//     li.nav__item
+//       a(href="//github.com/#{ social.github }", target="_blank"): i.fa.fa-github
+//     li.nav__item
+//       a(href="//codepen.io/#{ social.codepen }", target="_blank"): i.fa.fa-codepen
+//     li.nav__item
+//       a(href="//www.linkedin.com/in/#{ social.linkedin}", target="_blank"): i.fa.fa-linkedin-square
