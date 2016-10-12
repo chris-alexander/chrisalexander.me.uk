677b53161c9bfb431d8e34ef2c141ff760ed0d3c
Chris Alexander <git@chrisalexander.me.uk>
2016-10-12 20:46:16 +0100

    add Footer component

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="wrapper">
+      <p className="licence">
+        Copyright &copy 2016 Chris Alexander
+      </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
