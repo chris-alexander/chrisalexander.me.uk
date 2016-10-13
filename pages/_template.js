1eadfcf0a4adf4d9decdfa73c12f02a9292ea926
Chris Alexander <git@chrisalexander.me.uk>
2016-10-13 13:55:18 +0100

    add styling to Header

--- pages/_template.js
+++ pages/_template.js
@@ -4,29 +4,32 @@ import {Container} from 'react-responsive-grid';
 import {prefixLink} from 'gatsby-helpers';
 import {config} from 'config';
 
+import Header from 'components/Header';
+import 'tachyons';
+
 const Template = props => {
   const {location, children} = props;
-  let header;
-  if (location.pathname === prefixLink('/')) {
-    header = (
-      <h1>
-        <Link to={prefixLink('/')}>
-          {config.blogTitle}
-        </Link>
-      </h1>
-    );
-  } else {
-    header = (
-      <h3>
-        <Link to={prefixLink('/')}>
-          {config.blogTitle}
-        </Link>
-      </h3>
-    );
-  }
+  // let header;
+  // if (location.pathname === prefixLink('/')) {
+  //   header = (
+  //     <h1>
+  //       <Link to={prefixLink('/')}>
+  //         {config.blogTitle}
+  //       </Link>
+  //     </h1>
+  //   );
+  // } else {
+  //   header = (
+  //     <h3>
+  //       <Link to={prefixLink('/')}>
+  //         {config.blogTitle}
+  //       </Link>
+  //     </h3>
+  //   );
+  // }
   return (
     <Container>
-      {header}
+      <Header/>
       {children}
     </Container>
   );
