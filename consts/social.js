1cb987960a0f7ad60e3d00471f3f9fa0066ca5f3
Chris Alexander <git@chrisalexander.me.uk>
2016-10-16 16:49:43 +0100

    add icons to social links

--- consts/social.js
+++ consts/social.js
@@ -1,19 +1,29 @@
+import {
+  FaTwitter,
+  FaGithub,
+  FaCodepen,
+  FaLinkedin } from 'react-icons/lib/fa';
+
 const social = [
   {
     name: 'Twitter',
     link: 'https://twitter.com/ShutoTakahiro',
+    icon: FaTwitter,
   },
   {
     name: 'GitHub',
     link: 'https://github.com/chris-alexander',
+    icon: FaGithub,
   },
   {
     name: 'CodePen',
     link: 'https://codepen.io/chrisalexander',
+    icon: FaCodepen,
   },
   {
     name: 'LinkedIn',
     link: 'https://www.linkedin.com/in/calexanderuk',
+    icon: FaLinkedin,
   },
 ];
 
